86bae62b90a8c3e29cf3f79500c45bc397c238be
yusei88 <yuseisato1@gmail.com>
2024-01-08 17:08:54 +0900

    initial commit

---
+++ src/main/java/com/yusei/todo_api_java/TodoApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yusei.todo_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusei/todo_api_java/TodoApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusei.todo_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
